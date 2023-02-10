4e21c83d257ba1e1e54086ac0b1396cbe992bf9a
Roman <anomal3@yandex.ru>
2023-02-11 00:12:50 +0400

    Combine code

---
+++ charpcpp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace charpcpp
+{
+    internal class Program
+    {
+
+        [DllImport("cppcsharp.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void DisplayHello();
+
+        [DllImport("cppcsharp.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Multyply(float a, float b);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, this is DLL на языке C#\n");
+            DisplayHello();
+            Console.WriteLine("Теперь посчитаем на с++! Вводим два дробных числа. Ввели первое жмём enter");
+
+            float a = (float)Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+            Console.WriteLine();
+            float b = (float)Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+
+            Multyply(a, b);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ cppcsharp/cppcsharp.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "cppcsharp.h"
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+extern "C"
+{
+    __declspec(dllexport) void DisplayHello()
+    {
+        setlocale(LC_ALL, "Rus");
+        printf("Hello, this is DLL на языке c++\n");
+    }
+
+    __declspec(dllexport) void Multyply(float a, float b)
+    {
+        float c = a * b;
+        cout << "Ваш ответ из c++ " << c << endl;
+    }
+}
